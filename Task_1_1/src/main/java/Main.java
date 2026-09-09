99795f8f40cedf9767cfa9481b2abc98e9438c50
w1c1y1 <akulov.max3003@gmail.com>
2026-09-09 17:40:33 +0700

    javadoc added

--- Task_1_1/src/main/java/Main.java
+++ Task_1_1/src/main/java/Main.java
@@ -1,14 +1,33 @@
 public class Main {
+    /**
+     * Method for swapping elements of an array by their index
+     * @param array
+     * @param index1
+     * @param index2
+     */
     private static void swap(int[] array, int index1, int index2) {
         int temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
     }
 
+    /**
+     * Function to compare two elements
+     * @param array
+     * @param index1
+     * @param index2
+     * @return
+     */
     private static boolean compare(int[] array, int index1, int index2) {
         return array[index1] < array[index2];
     }
 
+    /**
+     * Method for shift down elements of an array with binary heap invariant
+     * @param array
+     * @param index
+     * @param len
+     */
     private void shiftDown(int[] array, int index, int len) {
         while (true) {
             int smallest = index;
@@ -30,6 +49,10 @@ public class Main {
         }
     }
 
+    /**
+     * heapSort implementation
+     * @param array
+     */
     public static void heapSort(int[] array) {
         int len = array.length;
         for (int i = len / 2 - 1; i >= 0; i--) {
@@ -39,7 +62,4 @@ public class Main {
             swap(array, 0, i);
             shiftDown(array, 0, i);
         }
-    }
-
-    public static void main(String )
 }
