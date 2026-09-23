e13cdd5d8175b9f99b0adb89d23875e16e1ffff7
w1c1y1 <w1c1y1@users.noreply.github.com>
2026-09-23 13:48:46 +0000

    Deploying to gh-pages from @ w1c1y1/syspro-OOP-2026@7734ff7438c2cdd42e9e558f1813546f24b57ec3 🚀

---
+++ Task_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"Dealer"},{"p":"<Unnamed>","l":"Deck"},{"p":"<Unnamed>","l":"Engine"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Player"},{"p":"<Unnamed>","l":"Rank"},{"p":"<Unnamed>","l":"Suits"}];updateSearchResults();
